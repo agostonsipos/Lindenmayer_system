3038d1e48b0b83292cfffbb8a418b8363f5a132b
Ágoston Sipos <agoston.sipos.95@gmail.com>
2020-01-05 01:13:20 +0100

    default positioning

--- main.cpp
+++ main.cpp
@@ -10,8 +10,6 @@
 #include <SDL2/SDL_opengl.h>
 #include <glm/glm.hpp>
 
-using namespace std;
-
 std::string Lindenmayer(std::string axiom, std::map<std::string, std::string> rules, size_t N)
 {
     std::string word = axiom;
@@ -34,7 +32,7 @@ std::string Lindenmayer(std::string axiom, std::map<std::string, std::string> ru
     return word;
 }
 
-std::pair<std::string,std::pair<int,int>> loadAndEval(std::string fname, int plusdetail = 0)
+std::pair<std::string,int> loadAndEval(std::string fname, int plusdetail = 0)
 {
     std::ifstream in(fname.c_str());
     int n, N, angle;
@@ -52,11 +50,12 @@ std::pair<std::string,std::pair<int,int>> loadAndEval(std::string fname, int plu
     in >> angle;
     N += plusdetail;
     assert(N >= 0);
-    return std::make_pair(Lindenmayer(axiom, rules, N), std::make_pair(1 << N, angle));
+    return std::make_pair(Lindenmayer(axiom, rules, N), angle);
 }
 
 struct state{
-    double xsv, ysv, xdsv, ydsv;
+    glm::vec2 pos;
+    glm::vec2 dir;
 };
 
 
@@ -66,15 +65,18 @@ struct Vertex
     glm::vec3 c;
 };
 
-std::tuple<GLuint, GLuint, size_t> genBuffer(std::pair<std::string,std::pair<int,int>> data)
+std::tuple<GLuint, GLuint, size_t> genBuffer(std::pair<std::string,int> data, glm::vec2 start)
 {
     std::string str = data.first;
-    int N = data.second.first;
-    int angleDeg = data.second.second;
+    int angleDeg = data.second;
     double angle = angleDeg*2*M_PI/360;
 
-    double x = 0, y = 0;
+    double x = start.x, y = start.y;
     double xd = 0.03, yd = 0;
+    glm::vec2 xy = start;
+    glm::vec2 dir = {0.03, 0};
+    
+    glm::mat2 rot = {cos(angle), -sin(angle), sin(angle), cos(angle)};
     
     std::stack<state> s;
     state st;
@@ -88,38 +90,32 @@ std::tuple<GLuint, GLuint, size_t> genBuffer(std::pair<std::string,std::pair<int
         {
             case 'F':
             case 'G':
-                vertices.push_back({glm::vec3(x,y,0), glm::vec3(0,1,0)});
-                vertices.push_back({glm::vec3(x+xd, y+yd,0), glm::vec3(0,1,0)});
-                x += xd;
-                y += yd;
+                vertices.push_back({glm::vec3(xy, 0), glm::vec3(0,1,0)});
+                vertices.push_back({glm::vec3(xy+dir, 0), glm::vec3(0,1,0)});
+                xy += dir;
                 break;
             case 'B':
-                x += xd;
-                y += yd;
+                xy += dir;
                 break;
             case '-':
-                a = xd;
-                xd = xd * cos(angle) - yd * sin(angle);
-                yd = a * sin(angle) + yd * cos(angle);
+				dir = glm::transpose(rot) * dir;
                 break;
             case '+':
-                a = xd;
-                xd = xd * cos(angle) + yd * sin(angle);
-                yd = -a * sin(angle) + yd * cos(angle);
+				dir = rot * dir;
                 break;
             case '[':
-                s.push(state{x,y,xd,yd});
+                s.push(state{xy, dir});
                 break;
             case ']':
                 st = s.top();
-                x = st.xsv; y = st.ysv; xd = st.xdsv; yd = st.ydsv;
+                xy = st.pos; dir = st.dir;
                 s.pop();
                 break;
             case 'X':
             case 'Y':
                 break;
             default:
-                cerr << "Unhandled character" << endl;
+                std::cerr << "Unhandled character" << std::endl;
         }
     }
     
@@ -162,13 +158,14 @@ std::tuple<GLuint, GLuint, size_t> genBuffer(std::pair<std::string,std::pair<int
     return {m_vaoID, m_vboID, vertices.size()};
 }
 
-int draw(std::pair<std::string,std::pair<int,int>> data)
+int draw(std::pair<std::string,int> data)
 {
     std::vector<std::string> examples{"cantor.txt", "sierpinski_tr.txt", "sierpinski_crp.txt", "dragon.txt", "koch.txt", "plant.txt"};
-    std::vector<int> scalefactors{3, 2, 3, 1, 3, 2};
+    std::vector<glm::vec3> defaultSettings{{-0.78, -0.16, 0.65},{-0.46, -0.4, 1.8},{-0.62, 0, 1.25},{0.02, 0.24, 1.2},{-0.78, -0.16, 0.64},{-0.78, -0.16, 0.6}};
+    std::vector<double> scalefactors{3, 2, 3, 1.2, 3, 2};
 
     std::string str = data.first;
-    int angleDeg = data.second.second;
+    int angleDeg = data.second;
     double angle = angleDeg*2*M_PI/360;
 
     SDL_Init(SDL_INIT_VIDEO);
@@ -207,15 +204,15 @@ int draw(std::pair<std::string,std::pair<int,int>> data)
     GLuint vao, vbo;
     size_t N;
 
-    std::tie(vao, vbo, N) = genBuffer(data);
+    std::tie(vao, vbo, N) = genBuffer(data, glm::vec2(0,0));
 
-    double size = 100;
-    int act = 4;
+    int act = 0;
     int plusdetail = 0;
     bool quit = false;
     SDL_Event event;
     double win_x = 1, win_y = 1;
-    double start_x = -1, start_y = 0;
+    double start_x = defaultSettings[act].x, start_y = defaultSettings[act].y;
+    double size = defaultSettings[act].z;
     while (!quit)
     {
         while(SDL_PollEvent(&event))
@@ -242,6 +239,9 @@ int draw(std::pair<std::string,std::pair<int,int>> data)
                             act = event.key.keysym.sym - 49;
                             plusdetail = 0;
                             data = loadAndEval("examples/"+examples[act], plusdetail);
+                            start_x = defaultSettings[act].x;
+                            start_y = defaultSettings[act].y;
+                            size = defaultSettings[act].z;
                             break;
                         case SDLK_p:
                             ++plusdetail;
@@ -267,13 +267,13 @@ int draw(std::pair<std::string,std::pair<int,int>> data)
                             break;
                     }
                     str = data.first;
-                    angleDeg = data.second.second;
+                    angleDeg = data.second;
                     angle = angleDeg*2*M_PI/360;
                     
                     glDeleteBuffers(1, &vao);
                     glDeleteVertexArrays(1, &vbo);
                     
-                    std::tie(vao, vbo, N) = genBuffer(data);
+                    std::tie(vao, vbo, N) = genBuffer(data, glm::vec2(0,0));
                     break;
                 case SDL_WINDOWEVENT:
                     if ( event.window.event == SDL_WINDOWEVENT_SIZE_CHANGED )
@@ -310,7 +310,7 @@ int draw(std::pair<std::string,std::pair<int,int>> data)
     return 0;
 }
 
-int main(int argc, char ** argv)
+int main(int argc, char** argv)
 {
     std::vector<std::string> examples{"cantor.txt", "sierpinski_tr.txt", "sierpinski_crp.txt", "dragon.txt", "koch.txt", "plant.txt"};
     return draw(loadAndEval("examples/"+examples[0]));
